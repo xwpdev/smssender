d00be969ffb29f6057c861b562f7ab189b933f41
xwpdev <zonixgate@gmail.com>
2017-09-09 23:08:46 +0530

    Add project files.

---
+++ SmsSender/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SmsSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Load = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ListView_Contacts = new System.Windows.Forms.ListView();
+            this.TextBox_Message = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Load
+            // 
+            this.btn_Load.Location = new System.Drawing.Point(12, 12);
+            this.btn_Load.Name = "btn_Load";
+            this.btn_Load.Size = new System.Drawing.Size(220, 23);
+            this.btn_Load.TabIndex = 0;
+            this.btn_Load.Text = "Load";
+            this.btn_Load.UseVisualStyleBackColor = true;
+            this.btn_Load.Click += new System.EventHandler(this.Btn_Load_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ListView_Contacts
+            // 
+            this.ListView_Contacts.CheckBoxes = true;
+            this.ListView_Contacts.Location = new System.Drawing.Point(12, 41);
+            this.ListView_Contacts.Name = "ListView_Contacts";
+            this.ListView_Contacts.RightToLeftLayout = true;
+            this.ListView_Contacts.Size = new System.Drawing.Size(220, 170);
+            this.ListView_Contacts.TabIndex = 1;
+            this.ListView_Contacts.UseCompatibleStateImageBehavior = false;
+            this.ListView_Contacts.View = System.Windows.Forms.View.List;
+            // 
+            // TextBox_Message
+            // 
+            this.TextBox_Message.Location = new System.Drawing.Point(12, 249);
+            this.TextBox_Message.MaxLength = 140;
+            this.TextBox_Message.Multiline = true;
+            this.TextBox_Message.Name = "TextBox_Message";
+            this.TextBox_Message.Size = new System.Drawing.Size(220, 172);
+            this.TextBox_Message.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add message";
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(12, 427);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(220, 23);
+            this.btn_Send.TabIndex = 4;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.Btn_Send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(244, 468);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox_Message);
+            this.Controls.Add(this.ListView_Contacts);
+            this.Controls.Add(this.btn_Load);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SMS Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Load;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView ListView_Contacts;
+        private System.Windows.Forms.TextBox TextBox_Message;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Send;
+    }
+}
+
---
+++ SmsSender/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using TextItWrapper;
+
+namespace SmsSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Load_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filename = openFileDialog1.FileName;
+                    string filelines = File.ReadAllText(filename);
+                    if (!string.IsNullOrEmpty(filelines))
+                    {
+                        var items = ListView_Contacts.Items;
+                        foreach (var number in filelines.Split(','))
+                        {
+                            if (!string.IsNullOrEmpty(number))
+                            {
+                                // check duplicates
+                                items.Add(number);
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void Btn_Send_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach (ListViewItem number in ListView_Contacts.CheckedItems)
+            {
+                var message = TextBox_Message.Text.Trim();
+                if (!SendSms(number.Text, message))
+                    continue;
+                else count++;
+            }
+            var dialogResult = MessageBox.Show(string.Format("{0} sms sent", count), "Success");
+            if (dialogResult == DialogResult.OK)
+            {
+                this.Controls.Clear();
+                this.InitializeComponent();
+            }
+        }
+
+        private bool SendSms(string number, string message)
+        {
+            Send send = new Send("94719110252", "5466", 1);
+            return send.SendSms(number, message);
+        }
+    }
+}
